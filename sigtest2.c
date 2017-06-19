b6401b11784530352c8b324936c9cc5be1463101
kaizoor <kai.schultz9612@gmail.com>
2017-06-19 19:08:41 +0200

    sigtest2

--- sigtest2.c
+++ sigtest2.c
@@ -10,37 +10,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <bits/sigset.h>
+#include <memory.h>
 
 volatile int SigNo;
 
 //Signalhandler
-void sigfunc(int sig) {
-	SigNo = sig;
+static void sigfunc(int sig) {
+    SigNo = sig;
+//    printf("%i\n", SigNo);
 }
 
-int main (void) {
-	SigNo = 0;
-    struct sigaction act;
+int main(void) {
+    SigNo = 0;
 
-    memset(&act, '\0', sizeof(act));
+    struct sigaction new_action, old_action;
 
-    act.sa_sigaction = &sigfunc;
-    act.sa_flags = SA_SIGINFO;
+    memset(&new_action, '\0', sizeof(new_action));
 
-	//irrelevant
-	printf("PID: %d\n", getpid());
+    new_action.sa_handler = &sigfunc;
 
-//	for (int i = 1; i < 33; i++){
-//		signal(i, sigfunc);
-//	}
-    if (sigaction(SIGTERM, &act, NULL) < 0) {
-        perror ("sigaction");
-        return 1;
+
+    for (int i = 1; i <= 34; i++) {
+        sigaction(i, NULL, &old_action);
+        if (old_action.sa_handler != SIG_IGN) {
+            sigaction(i, &new_action, NULL);
+        }
     }
-	sleep(60);
 
-	if (SigNo == 0) return 0;
-		else return SigNo;
+    sleep(60);
+
+    if (SigNo == 0) {
+        return 0;
+    } else {
+        return SigNo;
+    }
 }
 
