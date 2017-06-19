841ee050e72bbe3088c005e81085d8b3bfe2b89d
kaizoor <kai.schultz9612@gmail.com>
2017-06-19 18:07:21 +0200

    new sigstest

---
+++ sigtest3.c
@@ -0,0 +1,39 @@
+/*
+ * sigtest.c
+ *
+ *  Created on: 19.06.2017
+ *      Author: wolf
+ */
+
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+volatile int SigNo;
+
+//Signalhandler
+void sigfunc(int sig) {
+	SigNo = sig;
+}
+
+int main (void) {
+	SigNo = 0;
+
+	//irrelevant
+	printf("PID: %d\n", getpid());
+
+	/* signalhandler wird aufgerufen
+	 * d.h. die Funktion signal ruft bei einem abbruch die sigfunc auf
+	 * danach wird das Programm beendet
+	 */
+	for (int i = 1; i < 33; i++){
+		signal(i, sigfunc);
+	}
+	sleep(60);
+
+	if (SigNo == 0) return 0;
+		else return SigNo;
+}
+
