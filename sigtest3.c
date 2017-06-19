01b6e5a05668c58cff53f0d64393fd722b5de377
kaizoor <kai.schultz9612@gmail.com>
2017-06-19 19:50:20 +0200

    thats all I have

--- sigtest3.c
+++ sigtest3.c
@@ -5,35 +5,44 @@
  *      Author: wolf
  */
 
-
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <memory.h>
+#include <stdio.h>
 
 volatile int SigNo;
 
 //Signalhandler
-void sigfunc(int sig) {
-	SigNo = sig;
+static void sigfunc(int sig) {
+    SigNo = sig;
 }
 
-int main (void) {
-	SigNo = 0;
+int main(void) {
+    SigNo = 0;
 
-	//irrelevant
-	printf("PID: %d\n", getpid());
+    // PID ausgeben
+    printf("PID: %i\n", getpid());
 
-	/* signalhandler wird aufgerufen
-	 * d.h. die Funktion signal ruft bei einem abbruch die sigfunc auf
-	 * danach wird das Programm beendet
-	 */
-	for (int i = 1; i < 33; i++){
-		signal(i, sigfunc);
-	}
-	sleep(60);
+    struct sigaction new_action, old_action;
 
-	if (SigNo == 0) return 0;
-		else return SigNo;
+    memset(&new_action, '\0', sizeof(new_action));
+
+    new_action.sa_handler = &sigfunc;
+
+
+    for (int i = 1; i <= 34; i++) {
+        sigaction(i, NULL, &old_action);
+        if (old_action.sa_handler != SIG_IGN) {
+            sigaction(i, &new_action, NULL);
+        }
+    }
+
+    sleep(60);
+
+    if (SigNo == 0) {
+        return 0;
+    } else {
+        return SigNo;
+    }
 }
 
