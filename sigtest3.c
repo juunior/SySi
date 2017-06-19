a6976856b1c2f547032fd044ea19a1481b369b02
kaizoor <kai.schultz9612@gmail.com>
2017-06-19 23:44:37 +0200

    I think its okay

--- sigtest3.c
+++ sigtest3.c
@@ -10,29 +10,29 @@
 #include <sys/resource.h>
 
 
-volatile int SIGNO = 0;
+volatile int SigNo = 0;
 
-int ackermann(int n, int m) {
+int cpu_time(int n, int m) {
     if (n == 0) {
         return m + 1;
     } else if (m == 0) {
-        return ackermann(n - 1, 1);
+        return cpu_time(n - 1, 1);
     } else {
-        return ackermann(n - 1, ackermann(n, m - 1));
+        return cpu_time(n - 1, cpu_time(n, m - 1));
     }
 
 }
 
 static void sighandler(int signo) {
     printf("Signal empfangen: Nummer %d\n", signo);
-    SIGNO = signo;
-    if (SIGNO == SIGALRM) {
+    SigNo = signo;
+    if (SigNo == SIGALRM) {
         printf("Alarm wurde ausgeloest\n");
         return;
-    } else if (SIGNO == SIGXCPU) {
+    } else if (SigNo == SIGXCPU) {
         printf("CPU unter Last!\n");
         return;
-    } else if (SIGNO == SIGWINCH) {
+    } else if (SigNo == SIGWINCH) {
         printf("Fenstergroesse hat sich geÃ¤ndert!\n");
         return;
     }
@@ -70,13 +70,13 @@ int main(void) {
 
     //raise(SIGFPE); // division by 0 werfen
     printf("Nach Alarm in main\n");
-    //Ackermannfunktion aufrufen, Werte empirisch ermittelt
-    ackermann(3, 12);
-    printf("Ackermann beendet\n");
+    //CPU-Zeit generieren, Werte empirisch ermittelt
+    cpu_time(3, 12);
+    printf("CPU Zeit rlimit_cur erreicht --> SIGXCPU\n");
 
 
     //3tes Signal: FenstergÃroesse Ã¤ndern!
 
     sleep(60);  //Dauer = 60 Sekunden
-    return SIGNO;
+    return SigNo;
 }
